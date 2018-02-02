8f5be2104c42ed30470b6bd7242781dae6d5938d
root <root@localhost.localdomain>
2018-02-02 14:05:31 +0800

    filelock

--- filelock.c
+++ filelock.c
@@ -3,47 +3,54 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#define SEQFILE "./sequo" 
+#define SEQFILE "./sequo"
 #define MAXBUF 100
-main()
+main ()
 {
-	int		fd        , i, n, pid, seqno;
-	char		buff      [MAXBUF + 1];
-	pid = getpid();
-	if ((fd = open("data", O_RDWR | O_CREAT)) < 0) {
-		perror("Can't open");
-		exit(1);
+  int fd, i, n, pid, seqno;
+  char buff[MAXBUF + 1];
+  pid = getpid ();
+  if ((fd = open ("data", O_RDWR | O_CREAT)) < 0)
+    {
+      perror ("Can't open");
+      exit (1);
+    }
+  for (i = 0; i < 5; i++)
+    {
+      my_lock (fd);
+      lseek (fd, 01, 0);
+      if ((n = read (fd, buff, MAXBUF)) <= 0)
+	{
+	  perror ("read error");
+	  exit (1);
 	}
-	for (i = 0; i < 5; i++) {
-		my_lock(fd);
-		lseek(fd, 01, 0);
-		if ((n = read(fd, buff, MAXBUF)) <= 0) {
-			perror("read error");
-			exit(1);
-		}
-		buff[n] = 0;
-		if (!(n = sscanf(buff, "%d\n", &seqno))) {
-			perror("sscanf error");
-			exit(1);
-		}
-		printf("pid=%d,seq#=%d\n", pid, seqno);
-		seqno++;
-		sprintf(buff, "%03d\n", seqno);
-		n = strlen(buff);
-		lseek(fd, 01, 0);
-		if (write(fd, buff, n) != n) {
-			perror("write error");
-			exit(1);
-		}
-		my_unlock();
+      buff[n] = 0;
+      if (!(n = sscanf (buff, "%d\n", &seqno)))
+	{
+	  perror ("sscanf error");
+	  exit (1);
 	}
-	close(fd);
+      printf ("pid=%d,seq#=%d\n", pid, seqno);
+      seqno++;
+      sprintf (buff, "%03d\n", seqno);
+      n = strlen (buff);
+      lseek (fd, 01, 0);
+      if (write (fd, buff, n) != n)
+	{
+	  perror ("write error");
+	  exit (1);
+	}
+      my_unlock ();
+    }
+  close (fd);
 }
-my_lock(int fd)
+
+my_lock (int fd)
 {
-	return 0;
+  return 0;
 }
-my_unlock(int fd)
+
+my_unlock (int fd)
 {
-	return 0;
+  return 0;
 }
